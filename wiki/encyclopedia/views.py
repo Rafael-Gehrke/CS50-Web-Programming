97c711c3ec3cf7db41a7aaaf3b79f20d637e121c
Rafael Gehrke <rafaelgehrke0@gmail.com>
2024-07-28 09:47:24 -0300

    Index URLs work. Starting to implement search requests.

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from . import util
 
@@ -7,4 +7,15 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+    
+def entry_detail(request, entry_name):
+    entry_content = util.get_entry(entry_name)
+    return render(request, 'encyclopedia/entry_detail.html', {
+       'entry_content': entry_content,
+       'entry_name': entry_name,
+       "entries": util.list_entries()
+    })
 
+def search(request):
+    query = request.GET.get('q')
+    return redirect('entry_detail', slug=query)
