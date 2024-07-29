9e4a466574b512ce455c95902be59c117a75c17a
Rafael Gehrke <rafaelgehrke0@gmail.com>
2024-07-28 22:27:19 -0300

    Done search field.

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from django.http import Http404
 from . import util
 
 
@@ -17,5 +17,21 @@ def entry_detail(request, entry_name):
     })
 
 def search(request):
-    query = request.GET.get('q')
-    return redirect('entry_detail', slug=query)
+    query = request.GET.get('q','').strip().lower()
+    
+    # If the query matches the name of an encyclopedia entry, the user should be redirected to that entry’s page.
+    if util.get_entry(query):
+        return redirect('entry_detail', query)
+    # If the query does not match the name of an encyclopedia entry, the user should instead be taken to a search results page that displays a list of all encyclopedia entries that have the query as a substring. For example, if the search query were ytho, then Python should appear in the search results.
+    #substring_matches = []
+    entries = util.list_entries()
+    
+    substring_matches = [entry for entry in entries if query in entry.lower()]
+    print(f"Substring matches: {substring_matches}")
+    
+    return render(request, 'encyclopedia/substring_results.html', {
+        'substring_matches': substring_matches,
+        'query': query
+        })
+
+
