190449a8ee8e62267daf2c9159d24ccdcf83c521
Rafael <rafaelgehrke0@gmail.com>
2024-08-02 07:07:42 -0300

    Working on new page GET and POST methods

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, redirect
-from django.http import Http404
-from . import util
+from django.http import HttpResponseRedirect
 
+from . import util
+from django.urls import reverse
+from django import forms
+
+class NewPageForm(forms.Form):
+    title = forms.CharField(label="Title")
+    content = forms.Textarea()
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -26,7 +32,7 @@ def search(request):
     #substring_matches = []
     entries = util.list_entries()
     
-    substring_matches = [entry for entry in entries if query in entry.lower()]
+    substring_matches = [entry for entry in entries if query in entry.lower()] # list comprehension
     print(f"Substring matches: {substring_matches}")
     
     return render(request, 'encyclopedia/substring_results.html', {
@@ -35,4 +41,25 @@ def search(request):
         })
 
 def new_page(request):
+    if request.method == "POST":
+    # When the page is saved, if an encyclopedia entry already exists with the provided title, the user should be presented with an error message.
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            # Testar se a entry já existe!
+            if title in util.list_entries: # Entry já existe, erro! Verificar Case sensitivity??????????
+                return render(request, "encyclopedia/new_page.html", {
+                    "form": form
+                })
+            else:
+                util.save_entry(title, content)
+                return HttpResponseRedirect(reverse("encyclopedia/title"))
+        else: # ISSO???
+            return render(request, "encyclopedia/new_page.html", {
+                "form": form
+            })
+
     return render(request, "encyclopedia/new_page.html")
+    
+    # Otherwise, the encyclopedia entry should be saved to disk, and the user should be taken to the new entry’s page.
