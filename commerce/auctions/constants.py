6440820af87fe4c0f8006a173c4c252d1bedf2b6
Rafael Gehrke <rafaelgehrke0@gmail.com>
2025-01-13 19:28:08 -0300

    Project 2 - Commerce, DONE!

---
+++ commerce/auctions/constants.py
@@ -0,0 +1,19 @@
+CATEGORIES = [
+('', 'Select a category...'),
+('electronics', 'Electronics'),
+('fashion', 'Fashion'),
+('home_garden', 'Home & Garden'),
+('toys_hobbies', 'Toys & Hobbies'),
+('sports_outdoors', 'Sports & Outdoors'),
+('automotive', 'Automotive'),
+('health_beauty', 'Health & Beauty'),
+('books_movies_music', 'Books, Movies & Music'),
+('collectibles_antiques', 'Collectibles & Antiques'),
+('business_industrial', 'Business & Industrial'),
+('pet_supplies', 'Pet Supplies'),
+('real_estate', 'Real Estate'),
+('other', 'Other Categories'),
+]
+
+CATEGORY_DICT = dict(CATEGORIES)
+print(CATEGORY_DICT)
